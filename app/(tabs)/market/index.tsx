9a57ce374d273696e5111a40cd38b68dcfd15263
Shauryacious <shauryaagrawal2718@gmail.com>
2024-05-18 18:22:02 +0530

    home screen ui done with light and dark mode

--- app/(tabs)/market/index.tsx
+++ app/(tabs)/market/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import LottieView from "lottie-react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 function Market() {
   return (
-    <View className="flex items-center justify-center h-screen">
-      <Text>Market</Text>
-    </View>
+    <SafeAreaView>
+      <ThemedView className="flex items-center mt-14">
+        <ThemedView className="aspect-square h-96">
+          <LottieView
+            style={{ width: "100%", height: "100%" }}
+            source={require("@/assets/animations/welcome.json")}
+            autoPlay
+            loop
+          />
+        </ThemedView>
+        <ThemedText className="heading text-3xl font-bold">
+          Welcome to winCrypt
+        </ThemedText>
+        <ThemedText className="tagline text-xl text-center">
+          Invest your virtual ₹10,000 and compete with others
+        </ThemedText>
+      </ThemedView>
+    </SafeAreaView>
   );
 }
 
